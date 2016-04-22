6ccdda91cde799f76157b2d3ca13abdc00ed3a1a
someone12321 <mehandirattanikhil@gmail.com>
2016-04-23 00:41:11 +0530

    urls are extrated from the link

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-var request= require('request').defaults({proxy:'http://ipg_2012066:karankannu@192.168.1.107:3128/', agent:false});
+var request = require('request').defaults({proxy:'http://ipg_2012066:karankannu@192.168.1.107:3128/', agent:false});
 var cheerio = require('cheerio');
+var URL = require('url-parse');
 
-request('https://news.ycombinator.com', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    console.log(html);
+var START_URL = "https://www.rentomojo.com/";
+
+
+
+var links=[];
+
+function getUrls($,callback){
+  var link = $("a[href^='/']");
+  link.each(function() {
+      links.push($(this).attr('href'));
+
+  });
+  callback(links);
+}
+
+function urlss(data){
+  console.log(data);
+}
+
+request(START_URL, function (error, response, html) {
+  if(error){
+    console.log(error);
+  }else if (response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    
+      getUrls($,urlss);
   }
 });
