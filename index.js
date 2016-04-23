0ec2cbc5fa2ee3f58a60e12e5bc611814907a6d5
someone12321 <mehandirattanikhil@gmail.com>
2016-04-23 18:38:21 +0530

    1. About us and Contact Details is extracted from    Roposo.com 2. These are saved in About.txt and Contact.txt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var request = require('request').defaults({proxy:'http://ipg_2012066:karankannu@192.168.1.107:3128/', agent:false});
 var cheerio = require('cheerio');
 var URL = require('url-parse');
+var fs = require('fs');
 
 var START_URL = "http://www.roposo.com";
 
@@ -12,31 +13,36 @@ function getUrls($,callback){
   var link = $("a[href^='/']");
   link.each(function() {
       links.push($(this).attr('href'));
-
   });
   callback(links);
 }
 
 function urlss(data){
   for(var i=0,len=data.length;i<len;i++){
-    console.log(START_URL+data[i]);
-    if(START_URL+data[i] === "http://www.roposo.com/about"){
-      crawl(START_URL+data[i]);
+    // console.log(START_URL+data[i]);
+    if(START_URL+data[i] === "http://www.roposo.com/contact"){
+      crawlContact(START_URL+data[i]);
     }
+    if(START_URL+data[i] === "http://www.roposo.com/about"){
+      crawlAbout(START_URL+data[i]);
+    }
+
   }
 }
 
-request(START_URL, function (error, response, html) {
-  if(error){
-    console.log(error);
-  }else if (response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    
-      getUrls($,urlss);
-  }
-});
 
-function crawl(url){
+function crawlContact(url){
+  request(url,function(error,response,html){
+     if(error){
+      console.log(error);
+    }else if (response.statusCode == 200) {
+      var $ = cheerio.load(html);
+        getEmailPhone($,showContact);
+    } 
+  });
+}
+
+function crawlAbout(url){
   request(url,function(error,response,html){
      if(error){
       console.log(error);
@@ -47,15 +53,68 @@ function crawl(url){
   });
 }
 
+function getEmailPhone($,callback){
+  var z=String($.html());
+  var content=$(z).text();
+  content=content.replace('\n',"");
+  content = content.replace(/(\r\n|\n|\r)/gm,"");
+  var tex=content.split(" ");
+  var email=[];
+  var phone=[];
+  for(var i=0,len=tex.length;i<len;i++){
+    if(tex[i].match('[^@]+@[^@]+\.[^@]+')){
+      email.push(tex[i]);
+    }
+    if(tex[i].match('^[0-9()-]+$')){
+      if(tex[i].length==10){
+        // console.log(tex[i]);
+        phone.push(tex[i]);
+      }
+    }
+  }
+  callback(email,phone);
+}
+
+function showContact(data,data1){
+  var email="Email are :"+ data.join();
+  var phoneno="Phone No.:"+data1.join();
+  var contactDetais= email + '\n' +phoneno;
+  console.log("Going to write into a file");
+  fs.writeFile('Contact.txt', contactDetais,  function(err) {
+     if (err) {
+         return console.error(err);
+     }
+  console.log("Data written successfully!");
+  });
+}
 
 function getPtag($,callback){
   var ptag=[];
-  $('p').each(function(){
+  $('p').each(function(index){
+    ptag.push(index+":\n");
     ptag.push($(this).text());
   });
   callback(ptag);
 }
 
 function showPtag(data){
-  console.log(data);
+  console.log("Going to write into a file");
+  fs.writeFile('About.txt', data,  function(err) {
+     if (err) {
+         return console.error(err);
+     }
+  console.log("Data written successfully!");
+  });
+  // console.log(data);
 }
+
+
+request(START_URL, function (error, response, html) {
+  if(error){
+    console.log(error);
+  }else if (response.statusCode == 200) {
+    var $ = cheerio.load(html);
+      getUrls($,urlss);
+  }
+});
+
